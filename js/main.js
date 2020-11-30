7e128080bcb3b15dc690faf875069b90099f1745
DonatasGr <donatas888@gmail.com>
2020-11-30 05:47:42 +0200

    render socials validation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log('JS is running');
+import { renderSocials } from "./components/socials/renderSocials.js"
+import { socialsData } from './data/socialsData.js';
+
+renderSocials(socialsData);
+
+
+
+
+// const h1DOM = document.querySelector('h1');
+
+// const inputDOM = document.querySelectorAll('main .form > input');
+
+// console.log(h1DOM);
+// console.log(inputDOM);
+
+// h1DOM.innerText = 'Elgreko';
