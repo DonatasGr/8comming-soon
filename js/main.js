2632598d488afc405a366fe3f87f28fb8879b6c6
DonatasGr <donatas888@gmail.com>
2020-12-02 16:29:53 +0200

    Single progress bar validation and some JSDoc

--- js/main.js
+++ js/main.js
@@ -2,9 +2,16 @@ import { renderSocials } from "./components/socials/renderSocials.js"
 import { socialsData } from './data/socialsData.js';
 import { renderClock } from './components/clock/renderClock.js';
 
+import { renderAllProgressBars } from './components/progress-bar/renderAllProgressBars.js';
+import { progressBarData } from './data/progressBarData.js';
+
 renderSocials('footer > .row', socialsData);
 
 renderClock('.clock');
+
+renderAllProgressBars(progressBarData);
+
+/*renderProgressBar('.column.right', 'Lietuva', 100);*/
 
 // const h1DOM = document.querySelector('h1');
 
